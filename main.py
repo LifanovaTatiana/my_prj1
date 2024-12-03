3c419f74ee8d669c6b3c92d75487a990443da499
Tatiana Lifanova <lifanova_tv@mail.ru>
2024-12-03 12:07:59 +0200

    files

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.masks import card_number, get_mask_card_number
+
+print(get_mask_card_number(card_number))
+
+
+from src.masks import accound_number, get_mask_account
+
+print(get_mask_account(accound_number))
