3c61360c8837eb8c1d323c16b36faf2630f33ab3
Tatiana Lifanova <lifanova_tv@mail.ru>
2024-12-16 14:19:13 +0200

    add README.md

--- src/masks.py
+++ src/masks.py
@@ -12,5 +12,3 @@ def get_mask_account(number_account: int) -> str:
     """
     str_number_account = str(number_account)
     return f"**{str_number_account[-4:]}"
-
-
--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,20 @@
-def filter_by_state(dictionaries: Iterable[list[dict[Any, Any]]], state: Any='EXECUTED') -> list[list[dict[Any, Any]]]:
+from typing import Any, Iterable
+
+
+def filter_by_state(
+    dictionaries: Iterable[list[dict[Any, Any]]], state: Any = "EXECUTED"
+) -> list[list[dict[Any, Any]]]:
     """Функция выводит список словарей, у которых ключ 'state' соответствует заданному значению"""
 
     new_list_dict = []
     for i in dictionaries:
-        if i['state'] == state:
+        if i["state"] == state:
             new_list_dict.append(i)
     return new_list_dict
 
 
-def sort_by_date(dictionaries: Iterable[list[dict[Any, Any]]], reverse: bool=True) -> list[list[dict[Any, Any]]]:
+def sort_by_date(dictionaries: Iterable[list[dict[Any, Any]]], reverse: bool = True) -> list[list[dict[Any, Any]]]:
     """Функция возвращает список отсортированный по дате"""
 
-    sorted_list_dict = sorted(dictionaries, key=lambda x: x['date'], reverse=reverse)
+    sorted_list_dict = sorted(dictionaries, key=lambda x: x["date"], reverse=reverse)
     return sorted_list_dict
-
-
