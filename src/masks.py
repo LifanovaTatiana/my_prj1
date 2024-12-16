ba9c37babc74b67cb25f5a93aa4ef538f7fc4274
Tatiana Lifanova <lifanova_tv@mail.ru>
2024-12-16 22:34:14 +0200

    fixes feature/homework_10_1

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,4 @@
-def get_mask_card_number(number_card: int) -> str:
+def get_mask_card_number(number_card: str | int) -> str:
     """
     Функция маскировки номера банковской карты
     """
@@ -6,7 +6,7 @@ def get_mask_card_number(number_card: int) -> str:
     return f"{str_number_card[0:4]} {str_number_card[4:6]}** **** {str_number_card[-4:]}"
 
 
-def get_mask_account(number_account: int) -> str:
+def get_mask_account(number_account: str | int) -> str:
     """
     Функция маскировки номера банковского счета
     """
--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,20 @@
-from typing import Any, Iterable
+from typing import Dict, List
 
 
 def filter_by_state(
-    dictionaries: Iterable[list[dict[Any, Any]]], state: Any = "EXECUTED"
-) -> list[list[dict[Any, Any]]]:
+    dictionaries: List[Dict[str, str | int]], state: str = "EXECUTED"
+) -> List[Dict[str, str | int]]:
     """Функция выводит список словарей, у которых ключ 'state' соответствует заданному значению"""
 
-    new_list_dict = []
+    filtered_list = []
     for i in dictionaries:
         if i["state"] == state:
-            new_list_dict.append(i)
-    return new_list_dict
+            filtered_list.append(i)
+    return filtered_list
 
 
-def sort_by_date(dictionaries: Iterable[list[dict[Any, Any]]], reverse: bool = True) -> list[list[dict[Any, Any]]]:
+def sort_by_date(dictionaries: List[Dict[str, str | int]], reverse: bool = True) -> List[Dict[str, str | int]]:
     """Функция возвращает список отсортированный по дате"""
 
-    sorted_list_dict = sorted(dictionaries, key=lambda x: x["date"], reverse=reverse)
-    return sorted_list_dict
+    sorted_list = sorted(dictionaries, key=lambda x: x["date"], reverse=reverse)
+    return sorted_list
